c02f00868679e8332932690b6b720a4135bb4822
Curt Schroeders <curt.vaughn.schroeders@gmail.com>
2020-09-17 16:50:31 -0400

    Selenium Test File Initial

---
+++ SeleniumTest.java
@@ -0,0 +1,30 @@
+package selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class selentest {
+    public static void test() throws Exception {
+
+        //Set path
+        System.setProperty("webdriver.chrome.driver", "/Program Files (x86)/Google/Chrome/Application/chromedriver/chromedriver.exe");
+
+        //Initialize Browser
+        WebDriver driver=new ChromeDriver();
+
+        //Let the user see something 
+        Thread.sleep(5000);
+
+        //Open Chrome
+        driver.get("http://www.google.com");
+
+        //Close Chrome
+        driver.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        test();
+    }
+
+
+}
